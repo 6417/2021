18dc24a7820b14badf45097083db6035a76ca64f
Gian Laager <gianlaager114@gmail.com>
2021-02-22 21:56:54 +0100

    added FridolinsMotor interface

---
+++ src/main/java/frc/robot/utilities/fridolinsMotor/FridolinsMotor.java
@@ -0,0 +1,49 @@
+package frc.robot.utilities.fridolinsMotor;
+
+public interface FridolinsMotor {
+    public enum LimitSwitchPolarity {
+        kNormallyOpen, kNormallyClosed, kDisabled
+    }
+
+    public enum IdleModeType {
+        kBrake, kCoast
+    }
+
+    public enum FeedbackDevice {
+        QuadEncoder, CANEncoder, HallSensor
+    }
+
+    public enum DirectionType {
+        followMaster, invertMaster
+    }
+
+    public void set(double percent);
+
+    public void setPosition(int position);
+
+    public void enableForwardLimitSwitch(LimitSwitchPolarity polarity, boolean enable);
+
+    public void enableReverseLimitSwitch(LimitSwitchPolarity polarity, boolean enable);
+
+    public boolean isForwardLimitSwitchActive();
+
+    public boolean isReverseLimitSwitchActive();
+
+    public void setIdleMode(IdleModeType type);
+
+    public void follow(FridolinsMotor master, DirectionType direction);
+
+    public void setDirection(boolean forward);
+
+    public void setSensorDirection(boolean inverted);
+
+    public void setSensorPosition(double position);
+
+    public double getEncoderTicks();
+
+    public void factoryDefault();
+
+    public void configSelectedFeedbackSensor(FeedbackDevice device, int countsPerRev);
+
+    public void configOpenLoopRamp(double rate);
+}
