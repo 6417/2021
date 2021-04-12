f943fae9e7da35a3b1361b0b6c56c25e680fe703
Gian Laager <gianlaager114@gmail.com>
2021-04-12 15:44:05 +0200

    added selecPIDSlot to FridoTalonSRX

--- src/main/java/frc/robot/utilities/fridolinsMotor/FridoTalonSRX.java
+++ src/main/java/frc/robot/utilities/fridolinsMotor/FridoTalonSRX.java
@@ -23,7 +23,6 @@ public class FridoTalonSRX extends WPI_TalonSRX implements FridolinsMotor {
     private double kD;
     private double kF;
     private boolean isKFEnabled = false;
-    private Optional<Integer> selectedPIDSlot = Optional.empty();
     
     public FridoTalonSRX(int deviceID) {
 
