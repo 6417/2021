7cc37f518de9d22e64a6d4797b7b00ecdeb9e8e7
Gian Laager <gianlaager114@gmail.com>
2021-04-12 15:25:46 +0200

    added cruiseVelocity and acceleration

--- src/main/java/frc/robot/utilities/PIDValues.java
+++ src/main/java/frc/robot/utilities/PIDValues.java
@@ -8,6 +8,10 @@ public class PIDValues implements Cloneable {
     public double kP;
     public double kI;
     public double kD;
+    public double peakOutputReverse = -1;
+    public double peakOutputForward = 1;
+    public Optional<Double> cruiseVelocity;
+    public Optional<Double> acceleration;
 
     public PIDValues(double kP, double kI, double kD) {
         this.kP = kP;
@@ -22,13 +26,37 @@ public class PIDValues implements Cloneable {
         this.kF = Optional.of(kF);
     }
 
+    public PIDValues(double kP, double kI, double kD, double kF, double peakOutputReverse, double peakOutputForward) {
+        this.kP = kP;
+        this.kD = kD;
+        this.kI = kI;
+        this.kF = Optional.of(kF);
+        this.peakOutputReverse = peakOutputReverse;
+    }
+
+    /**
+     * @param cruiseVelocity the cruiseVelocity to set
+     */
+    public void setCruiseVelocity(double cruiseVelocity) {
+        this.cruiseVelocity = Optional.of(cruiseVelocity);
+    }
+
+    /**
+     * @param acceleration the acceleration to set
+     */
+    public void setAcceleration(double acceleration) {
+        this.acceleration = Optional.of(acceleration);
+    }
+
     @Override
     public PIDValues clone() {
         try {
             return (PIDValues) super.clone();
         } catch (CloneNotSupportedException e) {
-            PIDValues copy = new PIDValues(kP, kI, kD);
+            PIDValues copy = new PIDValues(kP, kI, kD, peakOutputReverse, peakOutputForward);
             kF.ifPresent((Double kF) -> copy.kF = Optional.of((double) kF)); // deep copying optional
+            cruiseVelocity.ifPresent((Double cruiseVelocity) -> copy.cruiseVelocity = Optional.of((double) cruiseVelocity)); // deep copying optional
+            acceleration.ifPresent((Double acceleration) -> copy.acceleration = Optional.of((double) acceleration)); // deep copying optional
             return copy;
         }
     }
