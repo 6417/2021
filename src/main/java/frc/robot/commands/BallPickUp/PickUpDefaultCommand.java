959115c1378f10721bf26470acd7967af4b6797a
Sebi <sebastian.rast@stud.schulegl.ch>
2021-04-26 16:46:39 +0200

    added pickUpDefaultCommand

---
+++ src/main/java/frc/robot/commands/BallPickUp/PickUpDefaultCommand.java
@@ -0,0 +1,15 @@
+package frc.robot.commands.ballPickUp;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.PickUpSubsystem;
+
+public class PickUpDefaultCommand extends CommandBase{
+    
+    PickUpDefaultCommand(){
+        addRequirements(PickUpSubsystem.getInstance()); 
+    }
+
+    @Override
+    public void 
+
+}
