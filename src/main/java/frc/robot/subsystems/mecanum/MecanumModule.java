cba262f34ec3b70d82c29d3a5ca7e74d01247e28
Gian Laager <gianlaager114@gmail.com>
2021-05-07 23:20:13 +0200

    added mountingPoint to configs

--- src/main/java/frc/robot/subsystems/mecanum/MecanumModule.java
+++ src/main/java/frc/robot/subsystems/mecanum/MecanumModule.java
@@ -2,12 +2,26 @@ package frc.robot.subsystems.mecanum;
 
 import java.util.function.Supplier;
 
+import edu.wpi.first.wpilibj.geometry.Translation2d;
 import frc.robot.utilities.fridolinsMotor.FridolinsMotor;
+import frc.robot.utilities.fridolinsMotor.FridolinsMotor.FeedbackDevice;
+import frc.robot.utilities.fridolinsMotor.FridolinsMotor.LimitSwitchPolarity;
 
 public class MecanumModule {
     public static class Config {
         public boolean motorInverted;
         public boolean encoderInverted;
         public Supplier<FridolinsMotor> motorInitializer;
+        public Translation2d mountingPoint;
+    }
+
+    public FridolinsMotor motor;
+
+    public MecanumModule(Config config) {
+        motor = config.motorInitializer.get();
+        motor.setInverted(config.motorInverted);
+        motor.setEncoderDirection(config.encoderInverted);
+        motor.configEncoder(FeedbackDevice.QuadEncoder, 1);
+        motor.enableForwardLimitSwitch(LimitSwitchPolarity.kNormallyClosed, false);
     }
 }
