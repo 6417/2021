e8a387e33fb95b9640ade194db22ed204315161f
Gian Laager <gianlaager114@gmail.com>
2021-05-29 15:22:10 +0200

    added bat file to compile BallColorUI to exe

---
+++ BallColorUI/BallColorUIWrapper.cpp
@@ -0,0 +1,5 @@
+#include <cstdlib>
+int main()
+{
+	std::system("C:\\Users\\Public\\wpilib\\2020\\jdk\\bin\\java.exe \"-javaagent:C:\\Program Files\\JetBrains\\IntelliJ IDEA 2020.3.2\\lib\\idea_rt.jar=62550:C:\\Program Files\\JetBrains\\IntelliJ IDEA 2020.3.2\\bin\" -Dfile.encoding=UTF-8 -classpath C:\\Users\\gian.l\\Documents\\Code\\2021\\BallColorUI\\out\\production\\BallColorUI ch.fridolins.client.Main");
+}
---
+++ BallColorUI/BuildBallColorUI.bat
@@ -0,0 +1,1 @@
+clang++ C:\Users\gian.l\Documents\Code\2021\BallColorUI\BallColorUIWrapper.cpp -o C:\Users\gian.l\Documents\Code\2021\BallColorUI\out\BallColorUI.exe
